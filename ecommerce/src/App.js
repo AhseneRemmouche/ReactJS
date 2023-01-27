85a79b77fdd0423933e0e4b33c5e9e8354dc2960
AhseneRemmouche <ahsene.remmouche@outlook.com>
2023-01-27 08:54:45 -0500

    Adding category-item and directory components

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -1,4 +1,5 @@
 import './categories.styless.scss';
+import Directory from './components/directory/directory.component.jsx';
 const App = () => {
     const categories = [
         {
@@ -27,22 +28,7 @@ const App = () => {
             imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
         },
     ];
-    return (
-        <div className='categories-container'>
-            {categories.map(({ id, title, imageUrl }) => (
-                <div key={id} className='category-container'>
-                    <div
-                        className='background-image'
-                        style={{ backgroundImage: `url(${imageUrl})` }}
-                    />
-                    <div className='category-body-container'>
-                        <h2>{title}</h2>
-                        <p>Shop Now</p>
-                    </div>
-                </div>
-            ))}
-        </div>
-    );
+    return <Directory categories={categories} />;
 };
 
 export default App;
