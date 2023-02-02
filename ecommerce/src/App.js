a33d9e1d7099aa71d96fa65802fce7ac7200aa35
AhseneRemmouche <ahsene.remmouche@outlook.com>
2023-02-01 19:30:37 -0500

    added styles

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -1,6 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './routes/home/home.component.jsx';
-import SignIn from './routes/sign-in/sign-in.component';
+import Authentication from './routes/authentication/authentication.component';
 import Navigation from './routes/navigation/navigation.component';
 
 const Shop = () => {
@@ -14,10 +14,21 @@ const Shop = () => {
 const App = () => {
     return (
         <Routes>
-            <Route path='/' element={<Navigation />}>
-                <Route index element={<Home />} />
-                <Route path='shop' element={<Shop />} />
-                <Route path='sign-in' element={<SignIn />} />
+            <Route
+                path='/'
+                element={<Navigation />}>
+                <Route
+                    index
+                    element={<Home />}
+                />
+                <Route
+                    path='shop'
+                    element={<Shop />}
+                />
+                <Route
+                    path='auth'
+                    element={<Authentication />}
+                />
             </Route>
         </Routes>
     );
