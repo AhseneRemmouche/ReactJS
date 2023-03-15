3c3a291d2699d86ba79b8dbf0b13590d603f0ea8
AhseneRemmouche <ahsene.remmouche@outlook.com>
2023-03-14 20:16:44 -0400

    added addToCart feature

---
+++ ecommerce/src/components/cart-item/cart-item.component.jsx
@@ -0,0 +1,21 @@
+import './cart-item.styles.scss';
+
+const CartItem = ({ cartItem }) => {
+    const { name, imageUrl, price, quantity } = cartItem;
+    return (
+        <div className='cart-item-container'>
+            <img
+                src={imageUrl}
+                alt={name}
+            />
+            <di className='item-details'>
+                <span className='name'>{name}</span>
+                <span className='price'>
+                    {quantity} x ${price}
+                </span>
+            </di>
+        </div>
+    );
+};
+
+export default CartItem;
